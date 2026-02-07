c99ef8f8a96f6f419758a736ad79176e173e9eeb
Sumit1694 <Sumitrane198@gmail.com>
2026-02-07 18:28:37 +0530

    Failed tests Xml based run by cross browser implemented

--- src/test/java/runners/FailedTestRunner.java
+++ src/test/java/runners/FailedTestRunner.java
@@ -1,16 +1,31 @@
 package runners;
 
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import utils.BrowserContext;
 
 @CucumberOptions(features="@target/failed_scenarios.txt",glue="stepDefinitions",monochrome=true,dryRun=false,
 plugin= {"html:target/cucumber.html","json:target/cucumber.json", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
 public class FailedTestRunner extends AbstractTestNGCucumberTests {
 
+	@BeforeTest(alwaysRun = true)
+    @Parameters("browser")
+    public void setBrowser(@Optional String browser) {
+
+        System.out.println(">>> Setting browser for thread: " + browser);
+
+        if (browser != null) {
+            BrowserContext.setBrowser(browser);
+        }
+    }
+
 	@Override
-	@DataProvider(parallel=true)
+	@DataProvider(parallel=false)
 	public Object[][] scenarios()
 	{
 		return super.scenarios();
